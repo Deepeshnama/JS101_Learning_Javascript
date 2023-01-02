506b8af81d8c5397fa48cad82f5c9b0c5a691b61
Deepesh Nama <deepeshnama0786@gmail.com>
2023-01-03 01:12:12 +0530

    add second day assignment

---
+++ ft-web-25-Logical-Operators-and-Ternary-Operator-IW/p4.js
@@ -0,0 +1,14 @@
+// Given 3 numbers (all different values), print which is greatest
+
+let a =2;
+let b =3;
+let c =1;
+
+
+if(a>b && a>c){
+  console.log("a is greatest")
+} else if (b>a && b>c){
+  console.log("b is greatest")
+} else {
+  console.log("c is greatest")
+}
